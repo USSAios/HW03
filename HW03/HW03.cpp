9a860dd48c09133a463f830d4556e0d54490de6f
USSA <ussa9905@gmail.com>
2025-06-09 14:37:40 +0900

    Update HW03.cpp

--- HW03/HW03.cpp
+++ HW03/HW03.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <iostream>
 #include <string>
+#include<algorithm>
 
 using namespace std;
 
@@ -9,29 +10,53 @@ template<typename T>
 class SimpleVector
 {
 private:
-	T* vec;
+	
 	int index = 0;
 	int cap = 0;
+
 public:
+	T* vec;
+
 	SimpleVector( int size = 10) { 
 
 		if (size <= 0) {
 			cout << "크기는 양수여야 합니다." << endl;
 			return;
 		}
-
 		vec = new T[size];
 		cap = size;
 	}
+
+	SimpleVector(const SimpleVector& svector) {
+		vec = svector.vec;
+	}
+
+
 	~SimpleVector() { delete[] vec; }
 
 	void push_back(const T& value)
 	{
 		if (index >= cap - 1) {
+
+			T* temp = vec;
+			vec = new T[index + 5];
+
+			for (int i = 0; i < index; i++) {
+				vec[i] = temp[i];
+			}
+
+			cap += 5;
+
+			push_back(value);
+
+			delete []temp;
+			
 			return;
 		}
-		index++;
+
 		vec[index] = value;
+		cout << vec[index] <<endl;
+		index++;
 	}
 
 	void pop_back()
@@ -43,6 +68,29 @@ public:
 		index--;
 	}
 
+	void resize(int size) 
+	{
+		if (size > cap - 1) {
+
+			T* temp = vec;
+			vec = new T[size];
+
+			for (int i = 0; i < index; i++) {
+				vec[i] = temp[i];
+			}
+
+			size = cap;
+
+			delete[]temp;
+		}
+	}
+
+	void sortData()
+	{
+
+		sort(vec, vec + index);
+	}
+
 	int size() {
 		return index;
 	}
@@ -51,23 +99,27 @@ public:
 		return cap;
 	}
 
+	void print() {
+		for (int i = 0; i < index; i++) {
+			cout << vec[i] << endl;
+		}
+	}
+
 };
 
 int main()
 {
-	SimpleVector<string> temp(5);
+	SimpleVector<int> temp(5);
 
-	cout << temp.size() << endl;
-	cout <<  temp.capacity() << endl;
+	for(int i = 6; i > 0; i--) {
+		temp.push_back(i);
+		//cout << temp.vec[i] << endl;
+		//cout << temp.capacity();
+	}
 
-	temp.push_back("a");
+	temp.sortData();
 
-	cout << temp.size() << endl;
-	cout << temp.capacity() << endl;
+	temp.print();
 
-	temp.pop_back();
-
-	cout << temp.size() << endl;
-	cout << temp.capacity() << endl;
-
+	cout << "현재 cap " << temp.capacity() << endl;
 }
